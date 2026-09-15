239a1c4177f4dc8d63aacd564a2de174f869dd55
Nacho <nacho@ies.local>
2026-09-15 10:03:04 +0200

    Auto-sync: Tue Sep 15 10:03:04     2026

---
+++ Unidad-02/PHP/02-Saludo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="UTF-8">
+      <title>Hola <?= $_GET["nombre"]; ?></title>
+   </head>
+   <body>
+      <h1>Hola Mundo!</h1>
+   </body>
+</html>
---
+++ Unidad-02/PHP/03-TiposDatos.php
@@ -0,0 +1,14 @@
+<?php
+$entero = 10; // Tipo de dato entero
+$decimal = 3.14; // Tipo de dato decimal (float)
+$cadena = "Hola, mundo"; // Tipo de dato cadena (string)
+$booleano = true; // Tipo de dato booleano
+$nulo = null; // Tipo de dato nulo
+
+echo "Tipo de dato entero: $entero <br>";
+echo "Tipo de dato decimal: $decimal <br>";
+echo "Tipo de dato cadena: $cadena <br>";
+echo "Tipo de dato booleano: $booleano <br>";
+echo "Tipo de dato nulo: $nulo <br>";
+
+?>
