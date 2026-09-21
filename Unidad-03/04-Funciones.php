171d5c586488a3702f273aa2378c45821bf756cc
Nacho <nacho@ies.local>
2026-09-21 10:28:02 +0200

    Auto-sync: Mon Sep 21 10:28:01     2026

--- Unidad-03/04-Funciones.php
+++ Unidad-03/04-Funciones.php
@@ -24,4 +24,22 @@ function miFun2() {
 // Llamada a la función y almacenamiento del valor devuelto
 $mensaje = miFun2();
 echo $mensaje;
+
+// Definición de una función con parámetros
+function miFun3($nombre, $edad, $estudiante = true) { // Parámetro con valor por defecto
+    echo "¡Hola, $nombre!, tienes $edad  y eres $estudiante<br>";
+}
+
+// Llamada a la función con un argumento
+echo "-> ";
+miFun3("Pedro",18); // Se pasan tres argumentos, pero la función solo utiliza el primero
+
+// Definición de una función con múltiples parámetros
+function miFun4($nombre) {
+    return "¡Hola, $nombre! Esta es una función con múltiples parámetros.<br>";
+}
+
+// Llamada a la función con un argumento y almacenamiento del valor devuelto
+$mensaje2 = miFun4("María");
+echo $mensaje2;
 ?>
