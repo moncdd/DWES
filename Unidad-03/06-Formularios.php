8a5ba1a56e45008568b5e026e4efc53c3f76eb1c
Nacho <nacho@ies.local>
2026-09-21 10:53:57 +0200

    Auto-sync: Mon Sep 21 10:53:57     2026

---
+++ Unidad-03/06-Formularios.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html lang="es">
+   <head>
+      <meta charset="UTF-8">
+      <title>Unidad 3 - Formularios</title>
+    </head>
+    <body>
+        <h1>Formulario base</h1>
+        <form action="06-RecogerDatos.php" method="get">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre">
+            <input type="submit" name="enviar" value="Enviar">
+        </form>
+    </body>
+</html>
